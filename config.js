73b1fa0262ff0c5b5eeccd266fc2ac93b1a97380
jinayPrajapati <jeenay2006@gmail.com>
2026-04-02 17:48:29 +0530

    Update config files

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ export const connectDB = () => {
     mongoose.connect(process.env.MONGO_URI).then(()=> {
         console.log("Database Connect Successfully");
     }).catch((err) => {
-        console.error(`Error : ${err}`)
+        console.error(`Error : ${err.message}`)
     })
 };
